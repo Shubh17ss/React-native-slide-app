6de0112b13f83f204ec0a29102d22b786177e8f3
Shubh17ss <shubhthe2001@gmail.com>
2022-09-25 15:44:05 +0530

    init

---
+++ components/CarsList/cars.js
@@ -0,0 +1,17 @@
+export default [{
+    name: 'Model S',
+    tagline: 'Starting at $69,420',
+    image: require('../../assets/images/ModelS.jpeg'),
+  }, {
+    name: 'Model 3',
+    tagline: 'Starting at $49,420',
+    image: require('../../assets/images/Model3.jpeg'),
+  }, {
+    name: 'Model X',
+    tagline: 'Starting at $99,420',
+    image: require('../../assets/images/ModelX.jpeg'),
+  }, {
+    name: 'Model Y',
+    tagline: 'Starting at $79,420',
+    image: require('../../assets/images/ModelY.jpeg'),
+  }];
